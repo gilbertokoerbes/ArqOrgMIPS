a8223cbd35760fc1e7ed2bf07a5be5f06a6f1521
gilbertokoerbes <72705755+gilbertokoerbes@users.noreply.github.com>
2021-05-09 16:16:25 -0300

    Update T1.asm
    recursion

--- T1.asm
+++ T1.asm
@@ -35,13 +35,13 @@ ult: .word 0								#Aponta para a ultima posição do array (a[length-1])
 rtnPosicao: .word 0								#Retorna o índice do valor correspondente
 meio: .word 0								
 size: .word 12								#Tamanho/Numero de itens do array
-	
+#msgSaida: .	
 	
 	.text
 	.globl main
 
 main:	
-#	##Carregando o Array A na pilha
+#	##Carregando o Array A na pilha (DESNECESSARIO)
 #	la $t0, A 		#Carrega o endereco do vetor A		
 #	lw $t1, size		#le o tamanho do array indicado	
 #	mul $t2, $t1, -4 	#multiplica o tamanho indicado por -4(espaço por valor)
@@ -63,31 +63,76 @@ main:
 #	next:
 	
 	# passagem de paremetros
+	li $a0, 0
+	li $a1, 11
+	li $a3, 9
 	
-	la $s0, A
-	la $a0, ($s0)
-	li $a1, 0 #valor do primeiro indice de busca
-	li $a2, 11 #le o valor do ultimo indice de busca
-	li $a3, 5 #valor a ser procurado
+	addi $sp, $sp, -16
+	la $t0, A
+	sw $t0, 12($sp)
+	sw $a0, 8($sp)
+	sw $a1, 4($sp)
+	sw $a3, 0($sp)
 	j binSearch
 	nextBin:
-	#syscall etc..	
+	lw $s0, ($v0)
+	
+	
 	
 	binSearch:
-	
-	bgt $a1, $a2, rtn1 #if (Prim > Ult)
+	lw $t8, 12($sp) #valor do endereco do array A
+	lw $t0,	8($sp)  # ler o valor do Prim
+	lw $t1, 4($sp) # ler o valor do Ult
+	lw $t9, 0($sp) #valor procurado
+	bgt $t0, $t1, rtn1 #if (Prim > Ult)
 	j else
 	rtn1:
 		li $v0, -1
 	j nextBin
 	
-	#else
-	else:	
-	add $t1, $a1, $a2 #soma primeiro e ultimo indice solicitado
-	srl $t2, $t1, 2	  #divide por 2 a soma
 	
-	move $t4, $zero #contador	
-	loopMeio: #procura se o valor esta no meio
+	#else
+	else:
+	add $t2, $t0, $t1 #soma prim e Ult
+	srl $t3, $t2, 1 # divide por dois para encontrar o Indice[meio]
+	mul $t6, $t3, 4 #multiplica o indice pelo tamanho de 4
+	lw  $t4, A($t3) #lê o valor[i(meio)] do array e armazena
+	lw $t5, 0($sp) #le o valor na base da pilha (valor procurado)
+	
+	#if
+	beq $t5, $t4, igualMeio
+	j elseIf 	#caso nao seja igual meio, pular para else if
+		igualMeio:
+			lw $v0, ($t3) #retorna a posição encontrada
+			j nextBin	
+	elseIf:
+		blt $t5, $t4, menorMeio
+		bgt $t5, $t4, maiorMeio
+		
+		menorMeio:
+			addi $sp, $sp, -16 	#libera espaço na pilha
+			
+			sw $t8, 12($sp) 	#posicao do array A
+			sw $t0, 8($sp) 		# valores Prim
+			addi $t3, $t3, -1	#atualizando o valor Ult
+			sw $t3, 4($sp)		#valor Ult[meio -1]
+			sw $t9, 0($sp)		#valor procurado
+			j binSearch
+		
+		maiorMeio:
+			addi $sp, $sp, -16 	#libera espaço na pilha
+			
+			sw $t8, 12($sp) 	#posicao do array A
+			addi $t3, $t3, 1	#atualizando o valor Ult
+			sw $t3, 8($sp) 		# valores Prim
+			sw $t1, 4($sp)		#valor Ult[meio -1]
+			sw $t9, 0($sp)		#valor procurado
+			j binSearch	
+			
+	
+	
+	
+	
 	
 	
 	
